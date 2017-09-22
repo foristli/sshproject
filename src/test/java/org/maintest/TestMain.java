7194740bfcee13f78847ca77303ce340ce0b4acc
foristli <864072603@qq.com>
2017-09-22 16:57:46 +0800

    tt

--- src/test/java/org/maintest/TestMain.java
+++ src/test/java/org/maintest/TestMain.java
@@ -7,7 +7,7 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ApplicationContext app=new ClassPathXmlApplicationContext("spring-hibernate.xml");
+		ApplicationContext app=new  ClassPathXmlApplicationContext("spring-hibernate.xml");
 	}
 
 }
